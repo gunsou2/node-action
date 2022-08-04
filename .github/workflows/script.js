494c27b38c8f2e605be7a2168948c50279759e85
Hayyi Gunsou <45726251+gunsou2@users.noreply.github.com>
2022-08-05 05:53:33 +0700

    Update script.js

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -3,10 +3,16 @@ const { Octokit } = require("@octokit/action");
 const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-// See https://developer.github.com/v3/issues/#create-an-issue
-const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
-  owner,
-  repo,
-  title: "My test issue",
-});
-console.log("Issue created: %s", data.html_url);
+(async() => {
+  console.log('before start');
+
+  // See https://developer.github.com/v3/issues/#create-an-issue
+  const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+    owner,
+    repo,
+    title: "My test issue",
+  });
+  console.log("Issue created: %s", data.html_url);
+  
+  console.log('after start');
+})();
